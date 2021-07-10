634236ad29af8fbb1082fe8391b729bc68af6b5d
Smurjo <Johanna@johanland.de>
2021-07-10 08:23:15 +0200

    Persistence between sessions and exit button added
    Persistence between sessions and exit button added, Both the start and the exit button are my own copyright.

--- Data-Persistence-Starter-Files/Assets/Scripts/MainManager.cs
+++ Data-Persistence-Starter-Files/Assets/Scripts/MainManager.cs
@@ -3,29 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEditor;
+using System.IO;
 
 public class MainManager : MonoBehaviour
-{  [SerializeField] BetweenScenesPersistanceSO betweenScenesPersistanceData;
+{
+    [SerializeField] BetweenScenesPersistanceSO betweenScenesPersistanceData;
     public Brick BrickPrefab;
     public int LineCount = 6;
     public Rigidbody Ball;
     public Text ScoreText;
+    public Text HighScoreText;
     public GameObject GameOverText;
-    
+
     private bool m_Started = false;
     private int m_Points;
-    
-    private bool m_GameOver = false;
 
-    
+    private bool m_GameOver = false;
+    private SaveData saveData;
+
     // Start is called before the first frame update
     void Start()
     {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            saveData = JsonUtility.FromJson<SaveData>(json);
+        }
+        else
+        {
+            saveData = new SaveData();
+            saveData.playerName = betweenScenesPersistanceData.playerName;
+            saveData.highScore = 0;
+        }
+        HighScoreText.text = "Best Score: " + saveData.playerName + ": " + saveData.highScore.ToString();
         ScoreText.text = "Score " + betweenScenesPersistanceData.playerName + ": 0";
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
+
+        int[] pointCountArray = new[] { 1, 1, 2, 2, 5, 5 };
         for (int i = 0; i < LineCount; ++i)
         {
             for (int x = 0; x < perLine; ++x)
@@ -65,12 +82,33 @@ public class MainManager : MonoBehaviour
     void AddPoint(int point)
     {
         m_Points += point;
-        ScoreText.text = "Score "+ betweenScenesPersistanceData.playerName + ": " + m_Points.ToString();
+        ScoreText.text = "Score " + betweenScenesPersistanceData.playerName + ": " + m_Points.ToString();
+        if (m_Points > saveData.highScore)
+        {
+            saveData.playerName = betweenScenesPersistanceData.playerName;
+            saveData.highScore = m_Points;
+            HighScoreText.text = "Best Score: " + saveData.playerName + ": " + saveData.highScore.ToString();
+        }
     }
 
     public void GameOver()
     {
+        string saveString = JsonUtility.ToJson(saveData);
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", saveString);
+
         m_GameOver = true;
         GameOverText.SetActive(true);
     }
+    public void ExitButtonPressed()
+    {
+        string saveString = JsonUtility.ToJson(saveData);
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", saveString);
+
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit(); 
+#endif
+    }
+
 }
